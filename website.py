6416351f7e61a5064f18c1625405dc21cdd61ede
Anoop Rajput <148322445+iamanoopraj@users.noreply.github.com>
2025-03-20 11:48:07 +0530

    Add files via upload

--- website.py
+++ website.py
@@ -1,4 +1,33 @@
 import streamlit as st
+from PIL.Image import Image
+from numpy.ma.core import left_shift
+from pandas.core.computation.align import align_terms
+
+st.markdown(
+    """
+    <style>
+    .stApp {
+        background-image: url("https://static.vecteezy.com/system/resources/previews/002/099/443/large_2x/programming-code-coding-or-hacker-background-programming-code-icon-made-with-binary-code-digital-binary-data-and-streaming-digital-code-vector.jpg");
+        background-size: cover;
+        background-position: center;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+st.header("My Profile")
+st.image("./anoop.jpg",caption="I am")
+
+st.header("Display video")
+video_file = open("./match.mp4","rb")
+video_bytes = video_file.read()
+
+st.video(video_bytes)
+
+
+
+
+
 
 
 name = st.text_input("Enter Your Name : ")
@@ -12,5 +41,21 @@ if button:
     Father: {Fname}
     Adress: {Adress}
     Qualification: {Qualification}""")
-    
+
+st.write("Please follow my instagram Account")
+from PIL import Image
+img = Image.open("its_raj09.lucky_qr.png")
+st.image(img)
+st.link_button("visit instagram","https://www.instagram.com/its_raj09.lucky/")
+st.link_button("visit GitHub","https://github.com/iamanoopraj")
+st.image("./follow.png",width=100)
+
+
+
+
+
+
+
+
+
 
